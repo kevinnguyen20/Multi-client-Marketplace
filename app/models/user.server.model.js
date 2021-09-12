b208e3f2d19edb991f794757bd8c812ad27f370b
kevinnguyen20 <kk.nguyen@web.de>
2021-09-12 18:29:33 +0200

    Comment out virtual setters

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -32,11 +32,11 @@ var UserSchema = new Schema({
 
 UserSchema.virtual('fullName').get(function() {
     return this.firstName + ' ' + this.lastName;
-}).set(function(fullName) {
+})/*.set(function(fullName) {
     var splitName = fullName.split(' ');
     this.firstName = splitName[0] || '';
     this.lastName = splitName[1] || '';
-});
+})*/;
 
 UserSchema.set('toJSON', { getters: true, virtuals: true });
 
