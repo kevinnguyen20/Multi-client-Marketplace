20c639a86770fc955e79ddce4101ab230a207716
kevinnguyen20 <kk.nguyen@web.de>
2021-09-13 00:37:12 +0200

    Add middleware for local passport

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,25 +1,32 @@
 var mongoose = require('mongoose'),
+    crypto = require('crypto'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
+    firstName: {
+        type: String,
+        required: 'First name is required'
+    },
+    lastName: {
+        type: String,
+        required: 'Last name is required'
+    },
     email: {
         type: String,
         index: true,
         unique: true,
-        required: true,
-        match: /.+\@.+\..+/
+        required: 'E-mail address is required',
+        match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
     },
     username: {
         type: String,
         trim: true,
         unique: true,
-        required: true
+        required: 'Username is required'
     },
     password: {
         type: String,
-        required: true,
+        required: 'Password is required',
         validate: [
             function(password) {
                 return password.length >= 6;
@@ -27,6 +34,15 @@ var UserSchema = new Schema({
             'Password should be longer than 5 chars'
         ]
     },
+    salt: {
+        type: String
+    },
+    provider: {
+        type: String,
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
     created: {
         type: Date,
         default: Date.now,
@@ -76,7 +92,48 @@ UserSchema.virtual('fullName').get(function() {
     this.lastName = splitName[1] || '';
 })*/;
 
-UserSchema.set('toJSON', { getters: true, virtuals: true });
+UserSchema.pre('save', function(next) {
+    if(this.password) {
+        this.salt = new
+            Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+
+    next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+    return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+
+    _this.findOne({
+        username: possibleUsername
+    }, function(err, user) {
+        if(!err) {
+            if(!user) {
+                callback(possibleUsername);
+            } else {
+                return _this.findUniqueUsername(username, (suffix || 0) +
+                    1, callback);
+            }
+        } else {
+            callback(null);
+        }
+    });
+};
+
+UserSchema.set('toJSON', { 
+    getters: true, 
+    virtuals: true 
+});
 
 mongoose.model('User', UserSchema);
 mongoose.model('Product', ProductSchema);
