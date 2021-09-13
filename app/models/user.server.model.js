1567c44312dcec72abd6428255c514169e369127
kevinnguyen20 <kk.nguyen@web.de>
2021-09-13 15:35:22 +0200

    Fix deprecated constructor

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -94,8 +94,9 @@ UserSchema.virtual('fullName').get(function() {
 
 UserSchema.pre('save', function(next) {
     if(this.password) {
-        this.salt = new
-            Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.salt = Buffer.alloc(
+            crypto.randomBytes(16).toString('base64'), 'base64'
+        );
         this.password = this.hashPassword(this.password);
     }
 
