11b24133c6746eb7a1881e6e7676570cd0ab79fe
kevinnguyen20 <kk.nguyen@web.de>
2021-09-16 16:59:57 +0200

    Add new profile page, reassign scrum roles

---
+++ app/controllers/profile.server.controller.js
@@ -0,0 +1,6 @@
+exports.render = function(req, res) {
+    // res.render('profile', {
+    //     title: 'The best prank stars',
+    //     userFullName: req.user ? req.user.fullName : ''
+    // });
+};
