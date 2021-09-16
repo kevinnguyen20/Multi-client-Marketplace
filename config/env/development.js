5232ce4647e44eeb9d0fa7d9bc59ff0c9ceffa19
kevinnguyen20 <kk.nguyen@web.de>
2021-09-16 08:55:38 +0200

    Add facebook login

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
     db: 'mongodb://localhost/serverdb',
-    sessionSecret: 'developmentSessionSecret'
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: 'Application Id',
+        clientSecret: 'Application Secret',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
 };
